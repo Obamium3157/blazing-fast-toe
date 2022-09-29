442406c44381f9d55c5d61ca4e0e102603261274
Obamium3157 <p_zhdanov@icloud.com>
2022-09-29 22:05:36 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+mod game;
+
+use game::input::Input;
+use game::Game;
+
+fn main() {
+    let mut game = Game::new();
+    game.paint(5, 5);
+
+    while game.get_in_game() {
+        let c = Input::input_usize();
+        let r = Input::input_usize();
+        game.turn(c, r);
+    }
+
+    if game.get_cause_of_end() == 1 {
+        println!("Draw!");
+    }
+    else if game.get_player() == 1 && game.get_cause_of_end() != 1 {
+        println!("First player won!");
+    }
+    else if game.get_player() == 2 && game.get_cause_of_end() != 1 {
+        println!("Second player won!");
+    }
+}
